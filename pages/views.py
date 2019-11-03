041765a01d22ef247bfeba59b088165f8b7f7be2
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-11-03 17:18:52 +0900

    영화 TOP 5 정보 가져오기 성공

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from datetime import datetime
+from .models import Movie
 
 # Create your views here.
+# http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101
 def index(request):
-    return render(request, 'pages/index.html')
+    url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
+    key="fab6fd6434401fd4302d3ebdab8fef88"
+    # today = DateFormat(datetime.now()).format('Ymd')
+    t = '20191102'
+
+
+
+    movie_url = f'{url}?key={key}&targetDt={t}'
+
+    data = requests.get(movie_url).json()
+    movies = data.get('boxOfficeResult').get('dailyBoxOfficeList')[:5]
+    rank = [ movies[i].get('rank') for i in range(0,5)]
+    mname = [ movies[i].get('movieNm') for i in range(0,5)]
+    openDt = [ movies[i].get('openDt') for i in range(0,5)]
+        
+
+    context = {
+        'rank' : rank,
+        'mname' : mname,
+        'openDt' : openDt,
+
+    }
+    return render(request, 'pages/index.html', context)
