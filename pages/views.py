84aa298918917eef2335b1682e3e206ef30acf07
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-12-13 23:35:34 +0900

    add movie detail

--- pages/views.py
+++ pages/views.py
@@ -9,7 +9,7 @@ def index(request):
     url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key="fab6fd6434401fd4302d3ebdab8fef88"
     # today = DateFormat(datetime.now()).format('Ymd')
-    t = '20191102'
+    t = '20191212'
 
 
 
@@ -18,14 +18,21 @@ def index(request):
     data = requests.get(movie_url).json()
     movies = data.get('boxOfficeResult').get('dailyBoxOfficeList')[:5]
     rank = [ movies[i].get('rank') for i in range(0,5)]
-    mname = [ movies[i].get('movieNm') for i in range(0,5)]
+    mname = [ (movies[i].get('movieNm'), movies[i].get('rank')) for i in range(0,5)]
     openDt = [ movies[i].get('openDt') for i in range(0,5)]
         
 
     context = {
+        'movies' : movies,
         'rank' : rank,
         'mname' : mname,
         'openDt' : openDt,
 
     }
     return render(request, 'pages/index.html', context)
+
+def detail(request):
+    context = {
+
+    }
+    return render(request, 'pages/detail.html', context)
